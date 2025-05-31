17895aefa61793e9f162a9b8f9d92e9884366ece
manue1666 <160285111+manue1666@users.noreply.github.com>
2025-05-30 23:36:51 -0600

    pequeño error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <>
             <div style={{ marginTop: "30px" }}>
                 <Container style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "50vh", textAlign: "center" }}>
-                    <h1>Bienvenido al generado de cotizaciones</h1>
+                    <h1>Bienvenido al generador de cotizaciones</h1>
                     <p>¿Necesitas un servicio personalizado? Obtén una cotización rápida y sin compromiso.
                         <br /> Selecciona el tipo de servicio, la complejidad y el plazo, y recibe un estimado en segundos.
                         <br /> ¡Haz tu solicitud ahora y lleva tu proyecto al siguiente nivel!</p>
